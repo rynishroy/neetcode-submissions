47db6913ab111134a0889e3e4bcffed56aa254d8
rynishroy <rynishroy@gmail.com>
2026-08-13 12:45:30 -0400

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        int[][] cars = new int[n][2];
+
+        for(int i=0;i<n;i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a,b) -> Integer.compare(b[0],a[0]));
+
+        Stack<Double> stack = new Stack();
+
+        for(int[] car:cars){
+            double time = (double)(target - car[0])/car[1];
+            // If current car reaches target later than fleet ahead,
+            // it cannot catch up, so it forms a new fleet.
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }
+        }
+        return stack.size();
+    }
+}
